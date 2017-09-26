fc393bbc6fe483653b9039a10ef5c2e8b9e84552
noeldanh <noeldan1@yahoo.com>
2017-09-26 15:35:42 -0700

    fix

--- server/models/user.js
+++ server/models/user.js
@@ -18,32 +18,31 @@ const UserSchema = new Schema({
 
 		},
 		password: {
-			type: String,
-			required: [true, "Please enter your password"],
-			minlength: [8, "Password needs to be at least 8 characters"],
-			maxlength: 32,
-			validate: {
-				validator: function( value ) {
-					return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,32}/.test( value );
-				},
-				message: "Password failed validation, you must have at least 1 number, uppercase and special character"
-	        }
-		},
+	    	type: String,
+	    	required: [true, "Password cannot be blank"],
+	    	minlength: [8, "Password needs to be at least 8 characters"],
+	    	validate: {
+	    		validator: function(value) {
+	    			return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,32}/.test(value);
+	    		},
+	    		message: "Password needs to have at least 1 number, uppercase, and special character"
+	    	}
+	    },
     }, { timestamps:true });
 
-UserSchema.pre('save',  function(next) {
-	if(this.password != this.password_confirmation){
+// UserSchema.pre('save',  function(next) {
+// 	if(this.password != this.password_confirmation){
 		// This line will create an error message that matches the format of our other error messages
-		this.invalidate('password', "Password and Password Confirmation must match");
+		// this.invalidate('password', "Password and Password Confirmation must match");
 		// In order for our pre-save method to fail we must pass an err into next()
-		let err = new Error("Password and password confirmation do not match");
-		next(err);
-	}else{
+	// 	let err = new Error("Password and password confirmation do not match");
+	// 	next(err);
+	// }else{
 		// remove password_confirmation so we're not storing it
-		this.password_confirmation = '';
-		this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
-		next();
-	}
-})
+// 		this.password_confirmation = '';
+// 		this.password = bcrypt.hashSync(this.password, bcrypt.genSaltSync(8));
+// 		next();
+// 	}
+// })
 
 mongoose.model('User', UserSchema);
